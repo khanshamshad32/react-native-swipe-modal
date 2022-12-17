e32464eb1598563452e4348a1f9701db2b088dae
Shamshad Khan <khanshamshad32@gmail.com>
2022-12-17 18:27:41 +0530

    add typescript

---
+++ index.ts
@@ -0,0 +1,8 @@
+/**
+ * @format
+ */
+
+import SwipeModalProvider from './SwipeModal/SwipeModalProvider';
+import {useSwipeModal} from './SwipeModal/SwipeModalContext';
+
+export {SwipeModalProvider, useSwipeModal};
